84ced662316a133e79c8a4d6e6ae0822bf53428f
Shubham Jha <shubham.jha2001m@gmail.com>
2020-09-01 19:19:03 +0700

    Projects

---
+++ src/components/Projects/Portfolio.js
@@ -0,0 +1,66 @@
+import p1 from '../../images/p1.png';
+import p2 from '../../images/p2.png';
+import p3 from '../../images/p3.jpg';
+import p4 from '../../images/p4.jpg';
+import p5 from '../../images/p5.jpg';
+import p6 from '../../images/p6.jpg';
+import p7 from '../../images/p7.jpg';
+import p8 from '../../images/p8.jpg';
+import p9 from '../../images/p9.jpg';
+
+export const portfolio = [
+    {
+      name: 'COVID-19 Tracker',
+      category: ['all', 'web'],
+      img: p3,
+      url: 'https://github.com/shubhamjha25/COVID-19-Tracker'
+    },
+    {
+      name: 'ToDo List',
+      category: ['all', 'react'],
+      img: p6,
+      url: 'https://github.com/shubhamjha25/ToDo-List'
+    },
+    {
+      name: 'User Registration System',
+      category: ['all', 'php'],
+      img: p5,
+      url: 'https://github.com/shubhamjha25/User-Registration-System'
+    },
+    {
+      name: 'Dictionary',
+      category: ['all', 'web'],
+      img: p4,
+      url: 'https://github.com/shubhamjha25/Dictionary'
+    },
+    {
+      name: 'Tic - Tac - Toe',
+      category: ['all', 'cpp'],
+      img: p1,
+      url: 'https://github.com/shubhamjha25/TIC-TAC-TOE'
+    },
+    {
+      name: 'Online Quiz (React & TS)',
+      category: ['all', 'react'],
+      img: p7,
+      url: 'https://github.com/shubhamjha25/Quiz-React-TS'
+    },
+    {
+      name: 'Portfolio Website',
+      category: ['all', 'react'],
+      img: p2,
+      url: 'https://shubhamjha25.github.io'
+    },
+    {
+      name: 'Bookstore Database',
+      category: ['all', 'php'],
+      img: p8,
+      url: '#'
+    },
+    {
+      name: 'Calendar (C - Graphics)',
+      category: ['all', 'cpp'],
+      img: p9,
+      url: 'https://github.com/shubhamjha25/Calendar-C-Graphics'
+    },
+  ];
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import './Projects.css'
+import { portfolio } from './Portfolio'
+
+function RenderCard({name, img, source}){
+    return(
+
+        <div className="card-wrap">
+            <a href={source} target="_blank"><img src={img} className="card-img" alt="Project-Image" /></a>
+            <h2 className="card-project-title">{name}</h2>
+        </div>
+
+    );
+}
 
 const Projects = () => {
+
+    const [filter, setFilter] = useState('all');
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        setProjects(portfolio);
+    }, []);
+
+    useEffect(() => {
+        setProjects([]);
+        const filtered = portfolio.map(p => ({ ...p, filtered: p.category.includes(filter) }));
+        setProjects(filtered);
+    }, [filter]);
+
     return (
         <>
-            <h2>Projects</h2>
+            <div className="portfolio__labels">
+                <a active={filter === 'all'} onClick={() => setFilter('all')}>ALL</a>
+                <a active={filter === 'web'} onClick={() => setFilter('web')}>HTML/CSS/JS</a>
+                <a active={filter === 'react'} onClick={() => setFilter('react')}>REACT</a>
+                <a active={filter === 'php'} onClick={() => setFilter('php')}>PHP</a>
+                <a active={filter === 'cpp'} onClick={() => setFilter('cpp')}>C/C++</a>
+            </div>
+
+            <div className="portfolio__container" style={{color: 'white'}}>
+                    {projects.map(item => item.filtered === true ? (
+                        <RenderCard name={item.name} img={item.img} source={item.url} />
+                    ) : '')}
+            </div>
         </>
     );
 }
